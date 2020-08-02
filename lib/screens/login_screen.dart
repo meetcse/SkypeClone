987bf8f1ae5409cf9343e3ffcbdf73c2ed761d49
meetcse <ymba.meet@gmail.com>
2020-08-03 00:53:34 +0530

    Added Google Sign in functionality using Firebase

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,60 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:skypeclone/resources/firebase_repository.dart';
+import 'package:skypeclone/screens/home_screen.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  FirebaseRepository _repository = FirebaseRepository();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: loginButton(),
+    );
+  }
+
+  Widget loginButton() {
+    return FlatButton(
+      padding: EdgeInsets.all(35),
+      child: Text(
+        "LOGIN",
+        style: TextStyle(
+            fontSize: 35, fontWeight: FontWeight.w900, letterSpacing: 2),
+      ),
+      onPressed: () => performLogin(),
+    );
+  }
+
+  void performLogin() {
+    _repository.signIn().then((FirebaseUser user) {
+      if (user != null) {
+        authenticateUser(user);
+      } else {
+        print("There was an error");
+      }
+    });
+  }
+
+  void authenticateUser(FirebaseUser user) {
+    _repository.authenticateUser(user).then((isNewUser) {
+      if (isNewUser) {
+        _repository.addDataToDb(user).then((value) {
+          Navigator.pushReplacement(context,
+              MaterialPageRoute(builder: (context) {
+            return HomeScreen();
+          }));
+        });
+      } else {
+        Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) {
+          return HomeScreen();
+        }));
+      }
+    });
+  }
+}
